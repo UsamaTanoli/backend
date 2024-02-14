8482d7ea3ca998d74d4956d6437768483e46569b
UsamaTanoli <usamakhan123451998@gmail.com>
2024-02-14 20:17:28 +0500

    Custom Api response and Errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import dotenv from "dotenv"
 import conntectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./env"
 })
 
 conntectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000);
+    console.log(`Server App is listning on PORT: ${process.env.PORT}`);
+})
+.catch((err) => {
+    console.log("MongoDb connection failed! :" ,err);
+})
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiRresponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = this.statusCode < 400
+    }
+}
